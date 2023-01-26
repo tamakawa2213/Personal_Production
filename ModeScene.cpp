afd778111ff26db0ef61cf9c77522a22857ac0c9
翔 玉河 <210306@jc-21.jp>
2023-01-26 11:38:01 +0900

    手応えなし

--- ModeScene.cpp
+++ ModeScene.cpp
@@ -13,8 +13,10 @@ ModeScene::~ModeScene()
 
 void ModeScene::Initialize()
 {
+	pText_ = new Text;
+	pText_->Initialize("test");
 	Direct3D::SplitScrMode = SCREEN_FULL;
-	Instantiate<ModeScene_UI>(this);
+	//Instantiate<ModeScene_UI>(this);
 }
 
 void ModeScene::Update()
@@ -23,4 +25,9 @@ void ModeScene::Update()
 
 void ModeScene::Release()
 {
+}
+
+void ModeScene::Draw()
+{
+	pText_->Draw();
 }
--- ModeScene.h
+++ ModeScene.h
@@ -1,8 +1,10 @@
 #pragma once
 #include "../IntegratedEngine/Engine/GameObject.h"
+#include "../IntegratedEngine/Engine/Text.h"
 
 class ModeScene : public GameObject
 {
+	Text* pText_;
 public:
 	ModeScene(GameObject* parent);
 
@@ -13,4 +15,6 @@ public:
 	void Update() override;
 
 	void Release() override;
+
+	void Draw() override;
 };
